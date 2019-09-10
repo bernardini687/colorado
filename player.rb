72e1dc5f97f1b959fc4d0e871d91c20e7b853785
Oscar Bernardini <bernardini687@gmail.com>
2019-09-10 18:00:45 +0200

    simplify collision detection

--- player.rb
+++ player.rb
@@ -10,16 +10,15 @@ class Player
   end
 
   def update(move_x)
-    move_x.positive? && move_x.times { would_fit(1, 0) && @x += 1 }
-    move_x.negative? && (-move_x).times { would_fit(-1, 0) && @x -= 1 }
+    move_x.positive? && move_x.times { @x += 1 if would_fit? 32 }
+    move_x.negative? && (-move_x).times { @x -= 1 if would_fit?(-1) }
   end
 
   def draw
-    @img.draw(x, y, 1)
+    @img.draw(@x, @y, 1)
   end
 
-  def would_fit(offs_x, offs_y)
-    !@map.solid?(@x + offs_x, @y + offs_y) # &&
-    # !@map.solid?(@x + offs_x, @y + offs_y - 45)
+  def would_fit?(offs_x)
+    !@map.solid?(@x + offs_x, @y)
   end
 end
